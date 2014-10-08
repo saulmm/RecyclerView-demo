454f6d325218150a3e3ade11cf865014669188bd
Saul M <saulmm2@gmail.com>
2014-10-08 21:10:29 +0200

    added fab button

--- app/src/main/java/saulmm/com/recyclerviewproject/MyActivity.java
+++ app/src/main/java/saulmm/com/recyclerviewproject/MyActivity.java
@@ -1,10 +1,12 @@
 package saulmm.com.recyclerviewproject;
 
 import android.app.Activity;
+import android.graphics.Outline;
 import android.os.Bundle;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 
 
 public class MyActivity extends Activity {
@@ -13,6 +15,13 @@ public class MyActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my);
+
+        int fabSize = getResources().getDimensionPixelSize(R.dimen.fab_size);
+        Outline fabOutline = new Outline();
+        fabOutline.setOval(0, 0, fabSize, fabSize);
+
+        View fabView = findViewById(R.id.fab_add);
+        fabView.setOutline(fabOutline);
 
         final RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
         recyclerView.setHasFixedSize(true);
